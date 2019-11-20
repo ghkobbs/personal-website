1953748c4c6485a46a60abc9405be308f0bca38e
Maxwell Morrison <maxkobbs@gmail.com>
2019-11-20 20:27:52 +0000

    Update (New): Created 2 new addtional pages

---
+++ src/pages/work.js
@@ -0,0 +1,14 @@
+import React from "react"
+import Layout from '../components/layout'
+import SEO from "../components/seo"
+
+const WorkPage = () => {
+    return (
+        <Layout>
+            <SEO title="Hello World" description="This is buitl wit gatsby" />
+            <div>Hello world!</div>
+        </Layout>
+    )
+}
+
+export default WorkPage
