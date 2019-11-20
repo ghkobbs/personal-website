93db4dfb63de977e2c5bbe20e669b18c03c29c86
Maxwell Morrison <maxkobbs@gmail.com>
2019-11-20 20:29:06 +0000

    Update (New): Created new components

---
+++ src/components/footer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <footer>
+            <p>Maxwell Morrison &copy;2019</p>
+            <small>This site is built with Hugo and hosted on Netlify. The source code is hosted on Github.</small>
+        </footer>
+    )
+}
+
+export default Footer
---
+++ src/components/layout.js
@@ -0,0 +1,29 @@
+import React from "react"
+import "../components/layout.module.scss"
+import Header from '../components/header'
+import Footer from '../components/footer'
+import Helmet from "react-helmet"
+
+const Layout = (props) => {
+    return (
+        <div>
+            <div className="main-content-group">
+                <div className="main-content-inner-wrapper">
+                    <Helmet>
+                    < link href = "https://fonts.googleapis.com/css?family=Alata|Assistant|Nunito+Sans&display=swap"
+                    rel = "stylesheet" />
+                    </Helmet>
+                    <Header />
+                    <div className="content-wrapper">
+                        <div className="content-inner-wrapper">
+                            {props.children}
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <Footer />
+        </div>
+    )
+}
+
+export default Layout
