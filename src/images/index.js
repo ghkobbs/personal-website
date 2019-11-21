7e869b2dae1bcc0e5e16134ec543ca966d537849
Maxwell Morrison <maxkobbs@gmail.com>
2019-11-21 00:14:59 +0000

    Update (fixed): Moved image folder outside

---
+++ src/images/index.js
@@ -0,0 +1,8 @@
+export const profilepic = require('./profile.png');
+export const nodejs = require('./nodejs.svg');
+export const css3 = require('./css3.svg');
+export const express = require('./express.svg');
+export const git = require('./git.svg');
+export const js = require('./javascript.svg');
+export const mongodb = require('./mongodb.svg');
+export const php = require('./php.svg');
