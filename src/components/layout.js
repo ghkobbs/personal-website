76b4a0b7a3a90918de3aacd8a246aaceb6acced6
Maxwell Morrison <maxkobbs@gmail.com>
2019-11-23 01:15:31 +0000

    Update: Added favicon

--- src/components/layout.js
+++ src/components/layout.js
@@ -4,6 +4,7 @@ import Styles from "../components/layout.module.scss"
 import Header from '../components/header'
 import Footer from '../components/footer'
 import Helmet from "react-helmet"
+import favicon from '../images/favicon.png'
 
 const Layout = (props) => {
     return (
@@ -11,8 +12,8 @@ const Layout = (props) => {
             <div className={Styles.mainContentGroup}>
                 <div className="main-content-inner-wrapper">
                     <Helmet>
-                    < link href = "https://fonts.googleapis.com/css?family=Alata|Assistant|Nunito+Sans&display=swap"
-                    rel = "stylesheet" />
+                        <link href = "https://fonts.googleapis.com/css?family=Alata|Assistant|Nunito+Sans&display=swap" rel = "stylesheet" />
+                        <link rel="icon" href={favicon} />
                     </Helmet>
                     <Header />
                     <div className="content-wrapper">
