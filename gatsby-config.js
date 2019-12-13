a56d2744f82d613d2c8011781af50ac1e843371e
Maxwell Morrison <maxkobbs@gmail.com>
2019-12-13 03:25:10 +0000

    Update: Made few changes to google analytics plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,6 +13,26 @@ module.exports = {
   },
   plugins: [
     {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        // The property ID; the tracking code won't be generated without it
+        trackingId: process.env.GOOGLE_ANALYTICS_TRACKING_ID,
+        // Defines where to place the tracking script - `true` in the head and `false` in the body
+        head: true,
+        // Setting this parameter is optional
+        anonymize: false,
+        // Setting this parameter is also optional
+        respectDNT: true,
+        // Avoids sending pageview hits from custom paths
+        exclude: ["/preview/**", "/do-not-track/me/too/"],
+        // Delays sending pageview hits on route update (in milliseconds)
+        pageTransitionDelay: 0,
+        // Any additional optional fields
+        siteSpeedSampleRate: 10,
+        cookieDomain: process.env.SITE_URL,
+      },
+    },
+    {
       resolve: `gatsby-plugin-sitemap`,
       options: {
         output: `/some-other-sitemap.xml`,
@@ -77,27 +97,6 @@ module.exports = {
           },
         ],
       },
-    },
-    {
-      resolve: `gatsby-plugin-google-analytics`,
-      options: {
-        // The property ID; the tracking code won't be generated without it
-        trackingId: process.env.GOOGLE_ANALYTICS_TRACKING_ID,
-        // Defines where to place the tracking script - `true` in the head and `false` in the body
-        head: true,
-        // Setting this parameter is optional
-        anonymize: false,
-        // Setting this parameter is also optional
-        respectDNT: true,
-        // Avoids sending pageview hits from custom paths
-        exclude: ["/preview/**", "/do-not-track/me/too/"],
-        // Delays sending pageview hits on route update (in milliseconds)
-        pageTransitionDelay: 0,
-        // Any additional optional fields
-        sampleRate: 5,
-        siteSpeedSampleRate: 10,
-        cookieDomain: process.env.SITE_URL,
-      },
-    },
+    }    
   ],
 }
--- src/components/footer.js
+++ src/components/footer.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import LayoutStyles from '../components/layout.module.scss'
 import { useStaticQuery, graphql } from "gatsby"
+import { OutboundLink } from 'gatsby-plugin-google-analytics'
 
 const Footer = () => {
     const { allContentfulSocialIcon } = useStaticQuery(
@@ -33,13 +34,13 @@ const Footer = () => {
                         {allContentfulSocialIcon.edges.map((edge) => {
                             return (
                                 <li key={edge.node.icon.file.url.toString()} className={LayoutStyles.socialIcons}>
-                                    <a href={edge.node.link} target="_blank" rel="noopener noreferrer" title={edge.node.title}><img src={edge.node.icon.file.url} alt={edge.node.title} /></a>
+                                    <OutboundLink href={edge.node.link} target="_blank" rel="noopener noreferrer" title={edge.node.title}><img src={edge.node.icon.file.url} alt={edge.node.title} /></OutboundLink>
                                 </li>
                             )
                         })}
                     </ul>
                     <p>&copy;2019 Maxwell Morrison. All rights reserved. </p>
-                    <small>This site is built with <a href="https://www.gatsbyjs.org/" className={LayoutStyles.link} target="_blank" rel="noopener noreferrer">Gatsbyjs</a> and hosted on <a href="https://www.netlify.com/" className={LayoutStyles.link} target="_blank" rel="noopener noreferrer">Netlify</a>. The source code can be found on <a href="https://github.com/ghkobbs/personal-website" className={LayoutStyles.link} target="_blank" rel="noopener noreferrer">Github</a>.</small>
+                    <small>This site is built with <OutboundLink href="https://www.gatsbyjs.org/" className={LayoutStyles.link} target="_blank" rel="noopener noreferrer">Gatsbyjs</OutboundLink> and hosted on <OutboundLink href="https://www.netlify.com/" className={LayoutStyles.link} target="_blank" rel="noopener noreferrer">Netlify</OutboundLink>. The source code can be found on <OutboundLink href="https://github.com/ghkobbs/personal-website" className={LayoutStyles.link} target="_blank" rel="noopener noreferrer">Github</OutboundLink>.</small>
                 </div>
             </div>
         </footer> 
